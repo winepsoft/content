bfbfab33ba44853dd5b486042cd4f123a97eaa50
shaisteS <shs.woem@gmail.com>
2016-08-21 14:34:04 +0430

    complete change them class

--- app/src/main/java/winep/ir/contentcentricapp/Utility/ChangeThem.java
+++ app/src/main/java/winep/ir/contentcentricapp/Utility/ChangeThem.java
@@ -15,8 +15,15 @@ public class ChangeThem {
     public final static int THEME_Green_Small_Size =110;
     public final static int THEME_Green_Meduim_Size =120;
     public final static int THEME_Green_large_Size =130;
-    public final static int THEME_BLUE = 2;
-    public final static int THEME_VIOLET = 3;
+    public final static int THEME_BLUE = 200;
+    public final static int THEME_BLUE_Small_Size =210;
+    public final static int THEME_BLUE_Meduim_Size =220;
+    public final static int THEME_BLUE_large_Size =230;
+    public final static int THEME_VIOLET = 300;
+    public final static int THEME_VIOLET_Small_Size =310;
+    public final static int THEME_VIOLET_Meduim_Size =320;
+    public final static int THEME_VIOLET_large_Size =330;
+
     /**
      * Set the theme of the Activity, and restart it by creating a new Activity of the same type.
      */
@@ -46,6 +53,24 @@ public class ChangeThem {
                 break;
             case THEME_Green_large_Size:
                 activity.setTheme(R.style.FirstTheme_LargeTextSize);
+                break;
+            case THEME_BLUE_Small_Size:
+                activity.setTheme(R.style.SecondTheme_SmallTextSize);
+                break;
+            case THEME_BLUE_Meduim_Size:
+                activity.setTheme(R.style.SecondTheme_MeduimTextSize);
+                break;
+            case THEME_BLUE_large_Size:
+                activity.setTheme(R.style.SecondTheme_LargeTextSize);
+                break;
+            case THEME_VIOLET_Small_Size:
+                activity.setTheme(R.style.ThirdTheme_SmallTextSize);
+                break;
+            case THEME_VIOLET_Meduim_Size:
+                activity.setTheme(R.style.ThirdTheme_MeduimTextSize);
+                break;
+            case THEME_VIOLET_large_Size:
+                activity.setTheme(R.style.ThirdTheme_LargeTextSize);
                 break;
         }
     }
